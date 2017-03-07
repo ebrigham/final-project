89b6262acf9f8a1c49cad76a2b6538a985cb0578
ebrigham <erbrigham@gmail.com>
2017-03-07 02:23:08 -0800

    it works!

--- js/java.js
+++
@@ -1,6 +0,0 @@
-function hideTitle(){
-    var p = document.querySelector(".navbar p");
-    var replace = p.replace("l0te's Art Gallery", " ");
-}
-var menu = document.getElementById("aria-expanded");
-menu.addEventListener("true", hideTitle);
